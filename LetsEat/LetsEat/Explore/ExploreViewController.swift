4b60beff9cd622b3270348431f87f257c5558100
Boris025 <98716155+Boris025@users.noreply.github.com>
2022-09-01 10:17:19 -0500

    First push
    Worked on the Launch screen and added tab view and collection view

---
+++ LetsEat/LetsEat/Explore/ExploreViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by Boris Ofon on 8/31/22.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController , UICollectionViewDataSource,
+                             UICollectionViewDelegate {
+    
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for:
+        indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind
+                        kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView =
+        collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind, withReuseIdentifier: "header",
+            for: indexPath)
+        return headerView
+    }
+    
+    
+
+}
